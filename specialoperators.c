72620563f89b9715195d97aeebf9e1e8e1822f8b
Lucas Barbosa <dasilvanetocelino@gmail.com>
2026-02-03 16:27:38 +0000

    Updt

--- specialoperators.c
+++ specialoperators.c
@@ -12,5 +12,28 @@ int main(void){
     char c = 'A';
     printf("The size of %c is %lu bytes\n", c, sizeof(c));
 
+    printf("------------------------------------------\n");
+    printf("\n");
+
+    printf("Address(&) and Pointer(*) -> Access address \n");
+
+    int *p = &a;
+
+    printf("The value of A is %d\n", a);
+    printf("The address of A is %p\n", &a);
+    printf("The value stored by the pointer is %p\n", p);
+    printf("The value acessed by the pointer is %d\n", *p);
+
+    *p = 10;
+    printf("The new value of A is %d\n", a);
+
+    printf("------------------------------------------\n");
+    printf("\n");
+
+    printf("Ternary operator\n");
+
+    int x = 7, y = 9;
+
+    printf("%d is greater than %d?: %s\n", x, y, (x > 7)?"Is Greater":"Is not greater");
     return 0;
 }
